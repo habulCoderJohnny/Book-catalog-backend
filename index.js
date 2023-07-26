305acd60ac85299656a283c0950ba268b7dbe57f
habulCoderJohnny <fokruljohnny@gmail.com>
2023-07-26 14:19:22 +0600

    Crud operation done of bookCollection

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
-const port = process.env.PORT || 5001;
+const port = process.env.PORT || 5000;
 
 const cors = require('cors');
 require('dotenv').config();
@@ -17,6 +17,44 @@ const run = async () => {
     try {
         const db = client.db('Boinama');
         const bookCollection = db.collection('books');
+
+        // bookCollection - CRUD operation
+        app.get('/books', async (req, res) => {
+            const cursor = bookCollection.find({});
+            const book = await cursor.toArray();
+            res.send({ status: true, data: book });
+        });
+        app.post('/book', async (req, res) => {
+            const book = req.body;
+            const result = await bookCollection.insertOne(book);
+            res.send(result);
+        });
+
+
+        app.post('/book', async (req, res) => {
+            const book = req.body;
+            const result = await bookCollection.insertOne(book);
+            res.send(result);
+        });
+        app.get('/book/:id', async (req, res) => {
+            const id = req.params.id;
+            const result = await bookCollection.findOne({ _id: ObjectId(id) });
+            res.send(result);
+        });
+
+        app.patch('/book/:id', async (req, res) => {
+            const id = req.params.id;
+            const updatedData = req.body
+            const result = await bookCollection.findOneAndUpdate({ _id: ObjectId(id) }, updatedData);
+            res.send(result);
+        });
+
+        app.delete('/book/:id', async (req, res) => {
+            const id = req.params.id;
+            const result = await bookCollection.deleteOne({ _id: ObjectId(id) });
+            res.send(result);
+        });
+
 
     } finally {
 
